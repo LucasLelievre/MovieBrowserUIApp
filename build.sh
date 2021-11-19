49ee9f8f4476b75a95bca9e51dafd58bbbb57f14
LucasLelievre <lucas.lelievre98@gmail.com>
2021-11-19 11:30:22 +0100

    launching app if build is successful

--- build.sh
+++ build.sh
@@ -3,3 +3,10 @@ mkdir build
 cd build
 cmake ..
 cmake --build . --config Release --clean-first
+if [ $? -eq 0 ]
+then
+    echo "build successful"
+    ./MovieBrowser
+else
+    echo "build failed" >&2
+fi

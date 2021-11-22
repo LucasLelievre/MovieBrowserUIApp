bcc0594cdb68a41d8d7dd3b8b30855d44507054e
LucasLelievre <lucas.lelievre98@gmail.com>
2021-11-22 02:01:41 +0100

    adding TV season data + front

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,6 @@ function refreshMovieCards(scannedData) {
                 break;
             case "tvseason":
                 url = "https://api.themoviedb.org/3/search/tv?api_key=39556c17fbe9e58c430f6a811f19fb1c&query="+encodeURIComponent(element.name);
-                console.log(url);
                 break;
             default:
                 url = "https://api.themoviedb.org/3/search/multi?api_key=39556c17fbe9e58c430f6a811f19fb1c&query="+encodeURIComponent(element.name);
@@ -69,11 +68,29 @@ function refreshMovieCards(scannedData) {
                 element["original_title"] = (element.type == "movie") ? firstResult.original_title : firstResult.original_name;
                 element["poster_path"] = firstResult.poster_path;
                 element["overview"] = firstResult.overview;
-                element["release_date"] = (element.type == "tvseason") ? firstResult.first_air_date : firstResult.release_date;
+
+                // If element is a TV season, look for more data
+                if (element.type == "tvseason") {
+                    return getDataFromURL("https://api.themoviedb.org/3/tv/"+element.id+"/season/"+element.season+"?api_key=39556c17fbe9e58c430f6a811f19fb1c")
+                    .then(function (result) {
+                        element["id"] = result.id;
+                        element["release_date"] = result.air_date;
+                        element["episodes"] = result.episodes;
+                        if (result.overview.length > 0) element["overview"] = result.overview;
+                        return element;
+                    }).catch(function(reason){
+                        // TODO create error card
+                        console.error("Could not retrieve TV season data :");
+                        console.error(reason);
+                    });
+                } else {
+                    element["release_date"] = firstResult.release_date;
+                }
                 return element;
             }).catch(function (reason) {
                 // TODO create error card
                 console.log(url);
+                console.error("A problem occured while retreive online data :");
                 console.error(reason);
                 //movieList.appendChild(createCard(0, "Error API", "img/missing_poster.png", reason, "error api"));
             })
@@ -81,6 +98,7 @@ function refreshMovieCards(scannedData) {
     });
     Promise.all(promises)
     .then(function (response) {
+        console.log(response);
         // sort JSON objects
         response.sort(sortByOriginalTitle);
         // Create and add the cards in html
@@ -88,29 +106,36 @@ function refreshMovieCards(scannedData) {
             movieList.appendChild(createCard(result, "https://image.tmdb.org/t/p/w500"));
         });
     }).catch(function (reason) {
+        console.error("A problem occured while create cards :");
         console.error(reason);
     });
 }
 
 // Create a card visualisation of a JSON object
-function createCard(movie, posterURL){
+function createCard(cardData, posterURL){
     let card = document.createElement("a");
     card.classList.add("card");
     card.classList.add("modalButton");
-    card.setAttribute("id", "card_"+movie.id);
+    card.setAttribute("id", "card_"+cardData.id);
         let cardPoster = document.createElement("img");
         cardPoster.classList.add("cardPoster");
         cardPoster.setAttribute("alt", "movie cardPoster");
-        cardPoster.setAttribute("src", posterURL+movie.poster_path);
+        cardPoster.setAttribute("src", posterURL+cardData.poster_path);
         let cardTitle = document.createElement("div");
         cardTitle.classList.add("cardTitle");
-        cardTitle.innerText = movie.original_title;
+        cardTitle.innerText = cardData.original_title;
     card.appendChild(cardPoster);
     card.appendChild(cardTitle);
-    if (movie.release_date != undefined) {
+    if (cardData.type == "tvseason") {
+        let cardSeason = document.createElement("span");
+        cardSeason.classList.add("cardTVep");
+        cardSeason.innerText = "Season "+cardData.season;
+        cardTitle.appendChild(cardSeason);
+    }
+    if (cardData.release_date != undefined) {
         let cardDate = document.createElement("div");
         cardDate.classList.add("cardDate");
-        cardDate.innerText = "(" + movie.release_date + ")";
+        cardDate.innerText = "(" + cardData.release_date + ")";
         card.appendChild(cardDate);
     }
     // Modal div control
@@ -118,7 +143,7 @@ function createCard(movie, posterURL){
 
     let modalDiv = document.createElement("div");
     modalDiv.classList.add("modalDiv");
-    modalDiv.setAttribute("id", "card_"+movie.id+"_modal");
+    modalDiv.setAttribute("id", "card_"+cardData.id+"_modal");
         let modalContent = document.createElement("div");
         modalContent.classList.add("modalContent");
             let mil = document.createElement("div");
@@ -127,19 +152,21 @@ function createCard(movie, posterURL){
             let mir = document.createElement("div");
             mir.classList.add("movieInfo");
                 let miTitle = document.createElement("h2");
-                miTitle.innerText = movie.original_title;
+                miTitle.innerText = cardData.original_title;
                 let miDescr = document.createElement("p");
                 miDescr.classList.add("description");
-                miDescr.innerText = movie.overview;
+                miDescr.innerText = cardData.overview;
+            mir.appendChild(miTitle);
+            mir.appendChild(miDescr);
+            if (cardData.type == "movie"){
                 let play = document.createElement("a");
                 play.classList.add("playButton");
                 //TODO play button for movies
                 play.setAttribute("id", "movie_file_name");
                 play.setAttribute("href", "#");
                 play.innerText = "play the movie";
-            mir.appendChild(miTitle);
-            mir.appendChild(miDescr);
-            mir.appendChild(play);
+                mir.appendChild(play);
+            }
         modalContent.appendChild(mil);
         modalContent.appendChild(mir);
     modalDiv.appendChild(modalContent);
@@ -170,11 +197,11 @@ function setEventListeners(){
     }
 }
 
-/*window.onload = function(){
+window.onload = function(){
     refreshMovieCards(
         "{\"data\":[{\"type\": \"movie\",\"name\": \"the matrix\",\"file name\": \"the matrix.mkv\"},\
         {\"type\": \"collection\",\"name\":\"james bond\",\"content\":[]},\
         {\"type\": \"tvshow\",\"name\": \"witcher\",\"file name\": \"witcher (TV)\"},\
-        {\"type\": \"tvseason\",\"name\": \"loki\",\"file name\": \"loki (S01)\"}]}");
+        {\"type\": \"tvseason\",\"name\": \"loki\", \"season\":\"1\",\"file name\": \"loki (S01)\"}]}");
     setEventListeners();
-};*/
+};
